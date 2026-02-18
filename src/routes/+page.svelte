610e644937b09fc5382b9289d3de8f0b1d6815ee
Sleepey09 <153181534+Sikae09@users.noreply.github.com>
2026-02-18 16:30:00 +0100

    feat: Added a function to add a TODO to the existing list (not persisted)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,20 @@
 		{ text: 'Todo 1', done: false },
 		{ text: 'Todo 2', done: false },
 	]);
+function addTodo(event: KeyboardEvent) {
+		if (event.key !== 'Enter') return
+
+		const todoEl = event.target as HTMLInputElement
+		const text = todoEl.value
+		const done = false
+
+		todos = [...todos, { text, done }]
+
+		todoEl.value = ''
+	}
 </script>
+<h1>Do-Too!</h1>
+<input onkeydown={addTodo} placeholder="Add todo" type="text" />
 
 <div class="todos">
 	{#each todos as todo}
